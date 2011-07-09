02fb0b86a4fd4f58736d2f6866450feed01129ed
Marco Dinacci <marco.dinacci@gmail.com>
2011-07-09 12:11:32 -0500

    Added image view.

--- gui.py
+++ gui.py
@@ -7,16 +7,25 @@ from PySide.QtGui import *
 
 import photoflow, config
 
-class MainWindow(QWidget):
+# TODO launch in a new thread the import process
+# every image processed send a signal from the import thread
+# and set the current filename here. 
+# Use a timer that every 20 ms or so refresh the image with the current one
+# as there's no need to resize ans show every photo, the human eya is not that fast
+class MainWindow(QMainWindow):
 
     _source = None
 
     def __init__(self):
-        QWidget.__init__(self, None)
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("Photoflow")
 
         vbox = QVBoxLayout()
 
         hbox = QHBoxLayout()
+       
+        # browse button and label
         self.dirLabel = QLabel("Select the directory containing the photos")
         self.dirBtn = QPushButton("Browse")
         self.dirBtn.clicked.connect(self._showImportDialog)
@@ -25,18 +34,37 @@ class MainWindow(QWidget):
         hbox.addWidget(self.dirBtn)
         vbox.addLayout(hbox)
 
+        # preview image
+        self.preview = QPixmap()
+        self._previewLabel = QLabel()
+        self._previewLabel.setPixmap(self.preview)
+        vbox.addWidget(self._previewLabel)
+
+        # import button
         self.importBtn = QPushButton("Import")
+        self.importBtn.setEnabled(False)
         self.importBtn.clicked.connect(self._import)
 
         vbox.addWidget(self.importBtn)
 
-        self.setLayout(vbox)
+        self.mainWidget = QWidget()
+        self.mainWidget.setLayout(vbox)
+        self.setCentralWidget(self.mainWidget)
+        self.resize(400, 300)
+
+    def update(self, filename):
+        self._currentImage = filename
+        img = QImage(filename)
+        self.preview = QPixmap.fromImage(img).scaledToWidth(380)
+        self._previewLabel.setPixmap(self.preview)
+        self._previewLabel.adjustSize()
 
     def _import(self):
         if self._source is not None:
             mm = photoflow.MediaManager()
             mm.set_source(self._source)
             mm.set_destination(config.PHOTOS_DESTINATION)
+            mm.register_subscriber(self)
             mm.import_photos()
 
             # TODO show progress
@@ -51,6 +79,7 @@ class MainWindow(QWidget):
         if directory:
             self._source = directory
             self.dirLabel.setText(directory)
+            self.importBtn.setEnabled(True)
 
 if __name__ == "__main__":
     import sys
